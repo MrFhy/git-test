7a2a5897a6bb7c717639c25b422d5221e73f33cb
fhy <fhy@java.com>
2021-07-22 23:07:30 +0800

    first commit

---
+++ git-demo/src/main/java/com/fhy/GitTest.java
@@ -0,0 +1,11 @@
+package com.fhy;
+
+/**
+ * @author fhy
+ * @create 2021-07-22 23:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git 1");
+    }
+}
